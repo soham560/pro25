66f9cf2c94a70b7dd1858a9bf9d20430361066da
soham560 <77228631+soham560@users.noreply.github.com>
2021-06-26 11:57:41 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,16 @@
+class paper {
+    constructor(x, y){
+        super(x,y,50,50);
+
+var options = {
+isStatic:false,
+restitution:0.3,
+friction:0,
+density:1.2,
+this:image = loadImage("sprites/paper.png"),
+
+}
+}
+}
+
+
---
+++ sketch.js
@@ -0,0 +1,41 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj,groundObject,paperObject
+var world;
+
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+	paperObject=new paper(200,200);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+ 
+
+  groundObject.display();
+  dustbinObj.display();
+  paperobject.display();
+  paper.display();
+  dustbin.display();
+  ground.display();
+
+}
+
